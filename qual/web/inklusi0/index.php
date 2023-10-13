b5fe150e2521af1f7623d3e915c6fdafd8c8dc39
Naufal Ardhani <naufalardhani89@gmail.com>
2023-10-13 19:15:20 +0700

    add inklusi0x01

---
+++ qual/web/inklusi0/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Inklusi0x0</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Inklusi0x0</h1>
+        <div class="file-options">
+            <a href="read_file.php?file=assets/txt/apa-itu-lfi.txt">apa-itu-lfi.txt</a>
+            <a href="read_file.php?file=assets/txt/menghindari-lfi.txt">menghindari-lfi.txt</a>
+            <!-- <a href="read_file.php?file=3.txt">Read 3.txt</a> -->
+            <!-- <a href="read_file.php?file=4.txt">Read 4.txt</a> -->
+        </div>
+    </div>
+</body>
+</html>
