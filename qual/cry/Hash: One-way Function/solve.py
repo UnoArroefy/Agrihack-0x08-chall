18749b182f8cff7f7bed03bbf3bfda8faa38e99f
azukagi-haru <ghifarazka21@gmail.com>
2023-10-28 23:56:25 +0700

    add new cry chall hash 3

---
+++ qual/cry/Hash: One-way Function/solve.py
@@ -0,0 +1,18 @@
+
+flags = open("flags.txt", "r").read().splitlines()
+real_hash = "32383630"
+
+# copy the hash function
+def my_hash(string):
+	sum = 0
+	for char in string:
+		sum += ord(char)
+	sum = sum % 2**24
+	return str(sum).encode().hex()
+
+
+for flag in flags:
+	hashed = my_hash(flag)
+
+	if hashed == real_hash:
+		print(flag)
