8e89d3d78c38e73fcb1f6ab46f1b7fb0f713600d
UnoArroefy <numroua@gmail.com>
2023-11-02 15:29:22 +0700

    add soal bang arai

---
+++ qual/web/time_to_sleep/README.md
@@ -0,0 +1,12 @@
+## Time to Sleep
+
+## Desc
+import request if its time to turu then give me flag
+<br>
+Reff: https://requests.readthedocs.io/en/latest/
+
+## Solution
+
+
+## Flag
+`agrihack{sweet_dr3ams_5}`
---
+++ qual/web/time_to_sleep/release_peserta.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0; 
+        }
+
+        .container {
+            background-color: #ffffff; 
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        form {
+            width: 100%;
+            max-width: 400px;
+        }
+
+        input[type="text"] {
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin-right: 10px;
+        }
+
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #007bff; 
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #0056b3; 
+        }
+
+        
+        .container {
+            font-family: 'Arial', sans-serif; 
+        }
+
+        label {
+            font-weight: bold; 
+            color: #333; 
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            <label for="flag">Flag : </label>
+            <input type="text" name="flag" id="flag">
+            <input type="submit" name="submit">
+        </form>
+    </div>
+</body>
+</html>
+
+
+<?php
+
+$flag = getenv("FLAG_ENV");
+if(isset($_POST['flag']))
+{
+	for($i = 0; $i < strlen($flag); $i++)
+	{
+		if($_POST['flag'][$i] == $flag[$i]){
+            if ($_POST['flag'] === $flag)
+                die("thats right");
+            sleep(1);
+        }
+		else{
+            die("Wrong!");
+        }
+			
+	}
+}
+?>
